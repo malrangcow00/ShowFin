6e5cc68866cf624db243d09b03a72c55e5f24618
JP <malrangcow00@gmail.com>
2023-11-16 17:33:36 +0900

    first commit on branch JP

--- final_pjt_back/articles/models.py
+++ final_pjt_back/articles/models.py
@@ -3,14 +3,21 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-
 class Article(models.Model):
+    category = models.CharField(max_length=20)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=20)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'{self.id}: {self.title}'
+        return f'{self.cateory}: {self.title} by {self.user}
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f'{self.article.pk}: {self.user} commented'
--- final_pjt_back/products/models.py
+++ final_pjt_back/products/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
+
 # Create your models here.
+class DepositProduct(models.Model):
+    fin_prdt_cd = models.TextField()
+    kor_co_nm = models.TextField()
+    fin_prdt_nm = models.TextField()
+    etc_note = models.TextField()
+    join_deny = models.IntegerField()
+    join_member = models.TextField()
+    join_way = models.TextField()
+    spcl_cnd = models.TextField()
+
+    def __str__(self):
+        return self.fin_prdt_nm
+
+
+class DepositOption(models.Model):
+    product_pk = models.ForeignKey(DepositProduct, on_delete=models.CASCADE)
+    fin_prdt_cd = models.TextField()
+    intr_rate_type_nm = models.TextField()
+    intr_rate = models.FloatField()
+    intr_rate2 = models.FloatField()
+    save_trm = models.IntegerField()
+
+
+class SavingProduct(models.Model):
+    fin_prdt_cd = models.TextField()
+    kor_co_nm = models.TextField()
+    fin_prdt_nm = models.TextField()
+    etc_note = models.TextField()
+    join_deny = models.IntegerField()
+    join_member = models.TextField()
+    join_way = models.TextField()
+    spcl_cnd = models.TextField()
+
+    def __str__(self):
+        return self.fin_prdt_nm
+
+
+class SavingOption(models.Model):
+    product_pk = models.ForeignKey(SavingProduct, on_delete=models.CASCADE)
+    fin_prdt_cd = models.TextField()
+    intr_rate_type_nm = models.TextField()
+    intr_rate = models.FloatField()
+    intr_rate2 = models.FloatField()
+    save_trm = models.IntegerField()
