1269720b51ecab2d695b774273fb5d1d4e1bf5ab
JP <malrangcow00@gmail.com>
2023-11-16 16:27:56 +0900

    make models

--- final_pjt_back/articles/models.py
+++ final_pjt_back/articles/models.py
@@ -1,15 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
 class Article(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField()
-    # image = models.ImageField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # like_users = models.ManyToManyField(User, related_name='like_articles')
-    # hashtags = models.ManyToManyField(Hashtag, related_name='articles', blank=True)
 
     def __str__(self):
         return f'{self.id}: {self.title}'
