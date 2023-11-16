b67435da940556308a713b2e7aebd0eb15e52e10
김태용 <94teo@naver.com>
2023-11-16 17:56:14 +0900

    User -> settings.AUTH_USER_MODEL 변경

--- final_pjt_back/articles/models.py
+++ final_pjt_back/articles/models.py
@@ -1,22 +1,22 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 
 # Create your models here.
 class Article(models.Model):
     category = models.CharField(max_length=20)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=20)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'{self.cateory}: {self.title} by {self.user}
+        return f'{self.cateory}: {self.title} by {self.user}'
 
 class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.CharField(max_length=30)
 
     def __str__(self):
