840a51a2a836df12562ab1872581e460fed03a10
JP <malrangcow00@gmail.com>
2023-11-17 17:46:40 +0900

    ad map, exchanger

--- final-pjt-front/src/views/SearchBankView.vue
+++ final-pjt-front/src/views/SearchBankView.vue
@@ -1,96 +1,174 @@
 <template>
-  <div>
-      <div id="map" style="width:100%;height:350px;"></div>
-  </div>
+    <div>
+        <h1>Search Bank</h1>
+        <label class="select">
+            <select name="city" id="city" v-model="selectedCityCode" @click="cityselect" required>
+                <option v-for="city in cities" :value="city.code" :key="city.name">
+                    {{ city.name }}
+                </option>
+            </select><span>　</span>
+        </label>
+        <label class="select">
+            <select name="district" id="district" @click="districtselect" required>
+                <option v-for="district in computedDistricts" :value="district" :key="district">
+                    {{ district }}
+                </option>
+            </select><span>　</span>
+        </label>
+        <!--<select name="bank" id="bank" required>-->
+        <!--    <option v-for="bank in banks" :value="bank" :key="bank"></option>-->
+        <!--    {{ bank }}-->
+        <!--</select><span>　</span>-->
+        <button @click="searchBanks">Search</button>
+    </div>
+    <br>
+    <div id="map"></div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
+import axios from 'axios';
 
-let map = null;
-let infowindow = null;
-let ps = null;
+const city_data = 'https://grpc-proxy-server-mkvo6j4wsq-du.a.run.app/v1/regcodes?regcode_pattern=*00000000'
 
-const initialMap = () => {
-    // 마커를 클릭하면 장소명을 표출할 인포윈도우 입니다
-    var infowindow = new kakao.maps.InfoWindow({zIndex:1});
-    // 지도를 표시할 div
-    var mapContainer = document.getElementById('map'),
-        mapOptions = {
-            // 지도의 초기 중심좌표
-            center: new kakao.maps.LatLng(37.566826, 126.9786567),
-            // 지도의 초기 확대 레벨
-            level: 3,
-    };
+const cities = ref([]);
+const selectedCityCode = ref(null);
+// const districts = ref([]);
 
-    // 지도를 생성합니다
-    var map = new kakao.maps.Map(mapContainer, mapOptions);
 
-    // 장소 검색 객체를 생성합니다
-    var ps = new kakao.maps.services.Places(map);
+const cityselect = () => {
+    axios({
+        //request
+        method: "get",
+        url: city_data,
+        responseType: "json",
+    })
+        .then(function (response) {
+            cities.value = response.data.regcodes;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
 
-    // 키워드로 장소를 검색합니다
-    ps.keywordSearch('서울특별시 강남구 국민은행', placesSearchCB);
+
+const district_data = 'https://grpc-proxy-server-mkvo6j4wsq-du.a.run.app/v1/regcodes?regcode_pattern={tmpcode}*00000';
+
+const computedDistricts = computed(() => {
+    const districtsArray = [];
+
+    if (selectedCityCode.value) {
+        const url = district_data.replace('{tmpcode}', selectedCityCode.value.substr(0, 2));
+
+        axios({
+            method: 'get',
+            url: url,
+            responseType: 'json',
+        })
+            .then(function (response) {
+                for (let i = 1; i < response.data.regcodes.length; i++) {
+                    const tmp = response.data.regcodes[i].name.split(' ')[1];
+                    if (!districtsArray.includes(tmp)) {
+                        districtsArray.push(tmp);
+                    }
+                }
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+    }
+
+    return districtsArray;
+});
+
+const searchBanks = () => {
+    const keyword = `${selectedCityCode.value} ${computedDistricts.value.join('-')}`;
+    console.log(keyword);
+    // Use the keyword for further processing or searching
 };
 
+const mapId = 'map';
+let infowindow = null;
+let map = null;
 
-// 키워드 검색 완료 시 호출되는 콜백함수 입니다
-function placesSearchCB (data, status, pagination) {
+const initMap = () => {
+    // Map container
+    const mapContainer = document.getElementById(mapId);
+
+    // Map options
+    const mapOption = {
+        center: new kakao.maps.LatLng(37.514575, 127.0495556),
+        level: 3,
+    };
+
+    // Create the map
+    map = new kakao.maps.Map(mapContainer, mapOption);
+
+    // Initialize the infowindow
+    infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
+
+    // Perform the place search
+    searchPlace('서울특별시 강남구 신한은행');
+};
+
+const searchPlace = (keyword) => {
+    const ps = new kakao.maps.services.Places();
+
+    // Keyword search
+    ps.keywordSearch(keyword, placesSearchCB);
+};
+
+const placesSearchCB = (data, status, pagination) => {
     if (status === kakao.maps.services.Status.OK) {
+        // Create bounds
+        const bounds = new kakao.maps.LatLngBounds();
 
-        // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
-        // LatLngBounds 객체에 좌표를 추가합니다
-        var bounds = new kakao.maps.LatLngBounds();
-
-        for (var i=0; i<data.length; i++) {
+        for (let i = 0; i < data.length; i++) {
             displayMarker(data[i]);
             bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
         }
 
-        // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+        // Set map bounds
         map.setBounds(bounds);
     }
-}
+};
 
-// 지도에 마커를 표시하는 함수입니다
-function displayMarker(place) {
-
-    // 마커를 생성하고 지도에 표시합니다
-    var marker = new kakao.maps.Marker({
+const displayMarker = (place) => {
+    const marker = new kakao.maps.Marker({
         map: map,
-        position: new kakao.maps.LatLng(place.y, place.x)
+        position: new kakao.maps.LatLng(place.y, place.x),
     });
 
-    // 마커에 클릭이벤트를 등록합니다
-    kakao.maps.event.addListener(marker, 'click', function() {
-        // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+    kakao.maps.event.addListener(marker, 'click', () => {
         infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
         infowindow.open(map, marker);
     });
-}
+};
 
-onMounted(async () => {
-  if (window.kakao && window.kakao.maps) {
-    initialMap();
-    console.log('if b',infowindow)
-  } else {
-    const script = document.createElement('script');
-    /* global kakao */
-    script.src = `//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${fbdd32eecea9ea022e10dec2254581c7}&libraries=services`;
-    document.head.appendChild(script);
-    script.onload = () => {
-      kakao.maps.load(initialMap);
-    }
-  }
-});
+onMounted(initMap);
 
 
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
 #map {
-  width: 400px;
-  height: 400px;
+    width: 800px;
+    height: 600px;
+}
+select {
+    text-align: right;
+}
+#city {
+    width: 120px;
+}
+#district {
+    width: 75px;
+}
+#bank {
+    width: 80px;
+}
+span {
+    font-size: 5px;
 }
 </style>
