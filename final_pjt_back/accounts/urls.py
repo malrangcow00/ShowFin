122c61aa48399b96b811c241d6f68d61fba1be7f
김태용 <94teo@naver.com>
2023-11-20 01:18:06 +0900

    장고 업데이트

--- final_pjt_back/accounts/urls.py
+++ final_pjt_back/accounts/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path,include
 from . import views
-from .views import CustomRegisterView, CustomUserDetailsView, change_password
+from .views import CustomRegisterView, CustomLoginView, CustomUserDetailsView, change_password
 from dj_rest_auth.registration.views import RegisterView
 from .serializers import CustomRegisterSerializer
 
 app_name = 'accounts'
 urlpatterns = [
     path('signup/', CustomRegisterView.as_view(), name='account_signup'),
+    # path('login/', CustomLoginView.as_view(), name='account_login'),
     path('user_detail/', CustomUserDetailsView.as_view(), name='account_userinfo'),
     path('user_info/', views.user_info, name="user_info"),
     path('', include('dj_rest_auth.urls')),
--- final_pjt_back/articles/urls.py
+++ final_pjt_back/articles/urls.py
@@ -5,7 +5,7 @@ app_name = 'articles'
 urlpatterns = [
     path('', views.article_list),
     path('<int:article_pk>/', views.article_detail),
-    path('<int:article_pk>/', views.article_like),
+    path('<int:article_pk>/like/', views.article_like),
     path('<int:article_pk>/comments/', views.comment_create),
     path('<int:article_pk>/comments/<int:comment_pk>/', views.comment_detail),
     path('<int:article_pk>/comments/<int:comment_pk>/like/', views.comment_like),
