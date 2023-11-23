b7c165fa1c7ca4bfd445f14f61f515f9f6e2eb6f
JP <malrangcow00@gmail.com>
2023-11-23 12:05:19 +0900

    update MainView.vue

--- final-pjt-front/src/views/MainView.vue
+++ final-pjt-front/src/views/MainView.vue
@@ -1,37 +1,87 @@
 <template>
-  <div>
-    <h1>MainView</h1>
+  <div class="carousel">
       <v-carousel
           cycle
-          height="800"
+          :height="500"
           hide-delimiter-background
-          show-arrows="hover"
+          :show-arrows="false"
+          class="carousel-inner"
       >
+          <a href="http://localhost:5173/products">
           <v-carousel-item
-              src="https://www.jungle.co.kr/image/ea06cd0346fa8777cb624e3f"
-              cover
+              src="../assets/main/comparing.png"
+              class="carousel-item"
           ></v-carousel-item>
+          </a>
 
+          <a href="http://localhost:5173/products">
           <v-carousel-item
-              src="https://blog.kakaocdn.net/dn/bKy6Mm/btqKfqu72Ga/05lByPgN2H0tYN3lHP3VU1/img.png"
-              cover
+              src="../assets/main/algo.jpg"
+              class="carousel-item"
           ></v-carousel-item>
+          </a>
 
+          <a href="http://localhost:5173/articles">
           <v-carousel-item
-              src="https://cdn.maily.so/7o7grtuc8937i1sd30x6ezf68b4g"
-              cover
+              src="../assets/main/event.jpg"
+              class="carousel-item"
           ></v-carousel-item>
+          </a>
       </v-carousel>
   </div>
+    <div class="bg-img">
+        <v-img
+            :width="1000"
+            :height="1000"
+            aspect-ratio="16/9"
+            src="../assets/main/bono.png"
+            class="bono"
+        ></v-img>
+        <v-img
+            :width="1000"
+            :height="1000"
+            aspect-ratio="16/9"
+            src="../assets/main/choonsik.png"
+            class="choonsik"
+        ></v-img>
+    </div>
 </template>
 
 
-<script setup></script>
+<script setup>
+
+</script>
 
 <style scoped>
-
-.carousel-inner > .item > img {
-    min-width: 50% !important;
-    min-height: 200px !important;
+.carousel {
+    display: flex;
+    justify-content: center;
+}
+a {
+    //width: 35%;
+    height: 100%;
+    z-index: 1;
+}
+.carousel-inner {
+    //width: 35%;
+    height: 100%;
+    z-index: 1;
+}
+.carousel-item {
+    //width: 50%;
+    height: 100%;
+    //z-index: 1;
+}
+.bono {
+    position: absolute;
+    top: 5%;
+    left: -15%;
+    //z-index: -1;
+}
+.choonsik {
+    position: absolute;
+    top: -10%;
+    left: 70%;
+    z-index: 1;
 }
 </style>
